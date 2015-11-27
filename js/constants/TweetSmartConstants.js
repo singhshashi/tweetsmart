df3b18090ca39c92ea78a73870c39b4c4ca585b2
Shashi Singh <singh.shashishekhar@gmail.com>
2015-11-27 20:13:24 +0530

    fixing error introduced in last checkin

--- js/constants/TweetSmartConstants.js
+++ js/constants/TweetSmartConstants.js
@@ -1,7 +1,6 @@
 
 module.exports = {
     BASE_API_URL:'http://api.tweetsmart.in/', 
-    TWEET_LENGTH:136 //Leaving four charaters to add numbering i.e. 140 - 4
     TWEET_LENGTH:136, //Leaving four charaters to add numbering i.e. 140 - 4
     KEY_TWEETSTORMTEXT: 'tweetstormtext',
     KEY_SIGNEDIN: 'signedIn',
